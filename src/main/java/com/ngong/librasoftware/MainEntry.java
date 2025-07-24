a36e096ee1c5f5b3da11ab9eae951a9516c5639b
ngong <joncopy7@gmail.com>
2025-07-24 04:53:09 +0200

    second last changes made

--- src/main/java/com/ngong/librasoftware/MainEntry.java
+++ src/main/java/com/ngong/librasoftware/MainEntry.java
@@ -1,10 +1,11 @@
 package com.ngong.librasoftware;
 
+import com.ngong.librasoftware.view.SplashScreen;
 import javafx.application.Application;
 
 public class MainEntry {
     public static void main(String[] args) {
-        Application.launch(SplashWindow.class,args);
+        Application.launch(SplashScreen.class,args);
     }
 
 }
